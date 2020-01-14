d1e02d2fdd3b6dfc9170dca410e977aa4cc039dc
Mikayla Davis <mikayla.lindsey.davis@gmail.com>
2020-01-14 08:07:42 -0800

    Loading after losing data to frozen R screen.

--- Code.R
+++ Code.R
@@ -1,12 +1,25 @@
-install.packages(haven)
+install.packages("haven")
 library(haven)
 library(tidyverse)
 library(dplyr)
 
 NFHS <- read_dta("IAHR52FL.dta")
-
 View(NFHS)
+
+# limited search from "hhid" to "shstruc" to reduce the variables I will be working with.
 hh <- select(NFHS, "hhid":"shstruc")
 
+# Filtering urban house type to analyse data for type of urban area and household size. 
+urban <- filter(hh, hv025 == 1)
+urban <- mutate(urban, Capital_large_city = hv026 == 0, Small_city = hv026 == 1, Town = hv026 == 2, Countryside = hv026 == 3)
 
+#Calculating the mean household size for urban houses in large cities.
+
+Large_city <- urban %>%
+  group_by(hv009) %>%
   
+  summaries(
+    filter(hv026 == 0),
+    mean = mean(hv009)
+  )
+
--- graph.R
+++ graph.R
@@ -1,2 +1,7 @@
-ggplot(NFHS, mapping = aes(hv009))+
+#Histogram plot of the distribution of the number of listed household members for the entire sample.
+ggplot(hh, mapping = aes(hv009))+
   geom_histogram(binwidth = 1)
+
+#Boxplot graph of the distribution of household size by type of urban area.
+ggplot(urban, mapping = aes (x = hv009, y = hv026))+
+  geom_boxplot()
